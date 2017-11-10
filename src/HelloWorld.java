257618fb105c8dcde468284d70361680cd268a73
Valdas <valdas@e-servisas.lt>
2017-11-10 19:53:55 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Labas pasaul i :)");
+
+        String pavadinimas = "UAB\t-tab-\\\\     \"Ragai ir kanopos\" \u265E";
+        System.out.println(pavadinimas);
+    }
+
+}
