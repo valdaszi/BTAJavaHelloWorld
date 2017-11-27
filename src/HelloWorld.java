3005333e0310644955535c8b9fd671bbfd7d2d5b
Valdas <valdas@e-servisas.lt>
2017-11-27 15:46:47 +0200

    Pataisiau pagal rekomendacijas

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Labas pasaul i :)");
 
-        String pavadinimas = "UAB\t-tab-\\\\     \"Ragai ir kanopos\" \u265E";
+        String pavadinimas = "UAB\t-tab-\\\\     \"Ragai ir kanopos\" ∞";
         System.out.println(pavadinimas);
 
         //TODO - cia bus kazkoks naujas kodas
